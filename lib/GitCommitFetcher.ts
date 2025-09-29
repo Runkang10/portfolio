84b5bcef82b4b41390103244d565ea3a1969d40e
Runkang10 <chen.runkang10@gmail.com>
2025-09-29 18:32:01 +0200

    refactor(lib): Update fetchCurrentGitCommit()

--- lib/GitCommitFetcher.ts
+++ lib/GitCommitFetcher.ts
@@ -1,5 +1,18 @@
-function fetchCurrentGitCommit(): string {
-  return "";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+function fetchCurrentGitCommit(): { branch: string; commit: string } {
+  const gitPath = join(process.cwd(), ".git");
+  const head = readFileSync(join(gitPath, "HEAD"), "utf8")
+    .trim()
+    .replace("ref: ", "");
+  const branch = head.replace("refs/heads/", "");
+  const commitPath = readFileSync(join(gitPath, head), "utf8");
+
+  return {
+    branch: branch,
+    commit: commitPath,
+  };
 }
 
 export default fetchCurrentGitCommit;
