71a2d12ce7d258840643e4fbfa930776da0b23a1
Runkang10 <chen.runkang10@gmail.com>
2025-10-16 12:33:54 +0200

    chore: Remove outdated fields

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  theme: {
-    extend: {
-      keyframes: {
-        gradient: {
-          "0%, 100%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
-        },
-      },
-      animation: {
-        gradient: "gradient 5s ease infinite",
-      },
-    },
-  },
 };
 
 export default config;
