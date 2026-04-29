710b797de9d93f72c32c1a5baec2f8acfdd10a8c
Runkang10 <chen.runkang10@gmail.com>
2026-04-29 17:08:58 +0200

    feat: Make contact links open on new tab

--- components/footer/sections/FooterContactSection.tsx
+++ components/footer/sections/FooterContactSection.tsx
@@ -28,7 +28,7 @@ const FooterContactSection = () => (
     <ul className="flex flex-col gap-2 text-xs">
       {Contacts.map((contact) => (
         <li key={contact.href}>
-          <Link href={contact.href}>
+          <Link href={contact.href} target="_blank" rel="noopener noreferrer">
             <div className="flex items-center gap-1 hover:text-muted-foreground">
               {contact.icon}
               {contact.label}
