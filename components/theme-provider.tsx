4600b1c1afa2b1d3a78998d05fbdcd2170a8e211
Runkang10 <chen.runkang10@gmail.com>
2026-04-30 15:10:26 +0200

    fix: Dark theme not being applied instantly

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -11,6 +11,7 @@ function ThemeProvider({
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
+      forcedTheme="dark"
       disableTransitionOnChange
       {...props}
     >
