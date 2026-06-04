d490db8b7fc3a266391c6add9ac649a131a160fa
Runkang10 <chen.runkang10@gmail.com>
2026-06-04 21:49:59 +0200

    refactor: Remove navigation links

--- components/Links.tsx
+++ components/Links.tsx
@@ -2,18 +2,7 @@ import Discord from "@/public/discord.svg"
 import Github from "@/public/github.svg"
 import NameMc from "@/public/namemc.png"
 
-const Links = [
-  {
-    label: "Projects",
-    href: "/projects",
-  },
-  {
-    label: "Blogs",
-    href: "/blogs",
-  },
-]
-
-const SocialLinks = [
+const socialLinks = [
   {
     icon: NameMc,
     href: "https://namemc.com/profile/Runkang10.1?q=72bcd535-9d39-407e-8cc2-bc7cf5b49714",
@@ -31,4 +20,4 @@ const SocialLinks = [
   },
 ]
 
-export { Links, SocialLinks }
+export { socialLinks }
