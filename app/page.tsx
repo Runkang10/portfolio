a7e109535f80a1f8e046e8b30fdaf9516d2df4c6
Runkang10 <chen.runkang10@gmail.com>
2026-06-04 21:49:12 +0200

    feat: New home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,44 @@
-import { Button } from "@/components/ui/button"
-
 export default function Page() {
   return (
-    <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-      <div>
-        <h1 className="font-medium">Project ready!</h1>
-        <p>You may now add components and start building.</p>
-        <p>We&apos;ve already added the button component for you.</p>
-        <Button className="mt-2">Button</Button>
+    <>
+      <div className="my-32 flex items-center justify-center">
+        <div className="flex flex-col gap-4 text-center">
+          <h1 className="text-5xl font-bold">Hi, I'm Runkang10</h1>
+          <p className="text-xl font-semibold text-muted-foreground">
+            Someone who can't finish a project...
+          </p>
+        </div>
       </div>
-      <div className="font-mono text-xs text-muted-foreground">
-        (Press <kbd>d</kbd> to toggle dark mode)
+      <div className="flex flex-col gap-6">
+        <h2 className="text-center text-2xl font-bold">Tech stacks</h2>
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <Section title="Kotlin" elements={["Paper API", "JDA"]} />
+          <Section
+            title="TypeScript"
+            elements={["HTML/CSS/JS", "Next.js", "Tailwind CSS"]}
+          />
+          <Section title="Go" elements={["Disgo"]} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
+
+const Section = ({
+  title,
+  elements,
+}: {
+  title: string
+  elements: string[]
+}) => (
+  <div className="flex justify-center">
+    <div className="flex flex-col gap-2">
+      <h3 className="text-lg font-medium">{title}</h3>
+      <ul className="space-y-1 text-sm">
+        {elements.map((element) => (
+          <li key={element}>{element}</li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
