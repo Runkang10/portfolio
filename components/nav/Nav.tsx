08d308dfcd80af13281e550240bbab989ce00a68
Runkang10 <chen.runkang10@gmail.com>
2026-06-04 21:51:41 +0200

    refactor: Remove navigation links and sidebar

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,15 +1,11 @@
 import NavBrand from "@/components/nav/NavBrand"
-import NavDesktopLinks from "@/components/nav/NavDesktopLinks"
-import { SideBar } from "@/components/nav/SideBar"
-import NavSocialLinks from "./NavSocialLinks"
+import SocialLinks from "../SocialLinks"
 
 const Nav = () => (
   <nav className="sticky top-0 left-0 z-50 m-0 flex h-16 content-center justify-between border-b backdrop-blur-lg">
     <NavBrand />
-    <NavDesktopLinks />
     <div className="content-center border-l px-4">
-      <NavSocialLinks autoHide={true} loadType="eager" />
-      <SideBar />
+      <SocialLinks autoHide={false} loadType="eager" />
     </div>
   </nav>
 )
