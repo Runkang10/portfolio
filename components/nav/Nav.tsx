b407e9b22422775598eb82bd887cf3f5472dfd59
Runkang10 <chen.runkang10@gmail.com>
2026-06-05 15:19:22 +0200

    refactor: Change backdrop blur

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -2,7 +2,7 @@ import NavBrand from "@/components/nav/NavBrand"
 import SocialLinks from "../SocialLinks"
 
 const Nav = () => (
-  <nav className="sticky top-0 left-0 z-50 m-0 flex h-16 content-center justify-between border-b backdrop-blur-lg">
+  <nav className="sticky top-0 left-0 z-50 m-0 flex h-16 content-center justify-between border-b backdrop-blur-md">
     <NavBrand />
     <div className="content-center border-l px-4">
       <SocialLinks autoHide={false} loadType="eager" />
